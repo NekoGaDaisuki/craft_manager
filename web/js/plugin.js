b9870e8ba3ad14887732cc6acb595dc0f9e8d0b2
NekoGaDaisuki <lexy.neko.ga.daisuki@gmail.com>
2016-07-22 19:35:11 +0200

    Refonte du formulaire en cours, commit d'ajout du formulaire réponsE.

---
+++ web/js/plugin.js
@@ -0,0 +1,19 @@
+function addRow(tableID)
+{
+    var table = document.getElementById(tableID);
+    var row = table.insertRow(table.rows.length);
+    for (var i = 0; i < table.rows[0].cells.length; i++)
+    {
+        var newcell = row.insertCell(i);
+        newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+    }
+}
+
+function deleteRow(tableID)
+{
+    var table = document.getElementById(tableID);
+    if (table.rows.length > 1)
+    {
+        table.deleteRow(table.rows.length - 1);
+    }
+}
--- web/js/script.js
+++ web/js/script.js
@@ -1,125 +1,48 @@
-function setup_form()
+function init_weapons_table()
 {
-    var root = $('#craft_form');
-    var last = $('#submit_field');
-
-    var weapons_count = -1;
-    $('#weapons_check').on('click', function () {
-        if (weapons_count == -1) {
-            weapons_count++;
-            var clone = $('#weapons_div').clone(true);
-            clone.attr('id', 'weapons_div_node');
-            last.before(clone);
-            $('#weapons_div_node #weapons_fieldset').children('#weapon_div').remove();
-        } else {
-            weapons_count = -1;
-            document.getElementById('weapons_div_node').remove();
-        }
-    });
-    $('#weapons_add_button').on('click', function () {
-        weapons_count++;
-        var clone = $('#weapons_div #weapons_fieldset #weapon_div').clone(true);
-        clone.attr('id', 'weapon_div'.concat(weapons_count));
-        clone.insertBefore('#weapons_div_node #weapons_fieldset #weapons_button_div');
+    $("#weapons_add_button").on("click", function ()
+    {
+        addRow("weaponsDataTable");
     });
 
-    var armors_count = -1;
-    $('#armors_check').on('click', function () {
-        if (armors_count == -1) {
-            armors_count++;
-            var clone = $('#armors_div').clone(true);
-            clone.attr('id', 'armors_div_node');
-            last.before(clone);
-            $('#armors_div_node #armors_fieldset').children('#armor_div').remove();
-        } else {
-            armors_count = -1;
-            document.getElementById('armors_div_node').remove();
-        }
+    $("#weapons_remove_button").on('click', function ()
+    {
+        deleteRow("weaponsDataTable");
     });
-    $('#armors_add_button').on('click', function () {
-        armors_count++;
-        var clone = $('#armors_div #armors_fieldset #armor_div').clone(true);
-        clone.attr('id', 'armor_div'.concat(armors_count));
-        clone.insertBefore('#armors_div_node #armors_fieldset #armors_button_div');
-    });
+}
 
-    var enchanting_count = -1;
-    $('#enchanting_check').on('click', function () {
-        if (enchanting_count == -1) {
-            enchanting_count++;
-            var clone = $('#enchanting_div').clone(true);
-            clone.attr('id', 'enchanting_div_node');
-            last.before(clone);
-            $('#enchanting_div_node #enchanting_fieldset').children('#glyph_div').remove();
-        } else {
-            enchanting_count = -1;
-            document.getElementById('enchanting_div_node').remove();
-        }
+function init_armors_table()
+{
+    $("#armors_add_button").on("click", function ()
+    {
+        addRow("armorsDataTable");
     });
-    $('#enchanting_add_button').on('click', function () {
-        enchanting_count++;
-        var clone = $('#enchanting_div #enchanting_fieldset #glyph_div').clone(true);
-        clone.attr('id', 'glyph_div'.concat(enchanting_count));
-        clone.insertBefore('#enchanting_div_node #enchanting_fieldset #enchanting_button_div');
+    $("#armors_remove_button").on("click", function ()
+    {
+        deleteRow("armorsDataTable");
     });
+}
 
-    var alchemy_count = -1;
-    $('#alchemy_check').on('click', function () {
-        if (alchemy_count == -1) {
-            alchemy_count++;
-            var clone = $('#alchemy_div').clone(true);
-            clone.attr('id', 'alchemy_div_node');
-            last.before(clone);
-            $('#alchemy_div_node #alchemy_fieldset').children('#potion_div').remove();
-        } else {
-            alchemy_count = -1;
-            document.getElementById('alchemy_div_node').remove();
-        }
+function init_enchanting_table()
+{
+    $("enchanting_add_button").on("click", function ()
+    {
+        addRow("enchantingDataTable");
     });
-    $('#alchemy_add_button').on('click', function () {
-        alchemy_count++;
-        var clone = $('#alchemy_div #alchemy_fieldset #potion_div').clone(true);
-        clone.attr('id', 'potion_div'.concat(alchemy_count));
-        clone.insertBefore('#alchemy_div_node #alchemy_fieldset #alchemy_button_div');
+    $("enchanting_remove_button").on("click", function ()
+    {
+        deleteRow("enchantingDataTable");
     });
+}
 
-    var cooking_count = -1;
-    $('#cooking_check').on('click', function () {
-        if (cooking_count == -1) {
-            cooking_count++;
-            var clone = $('#cooking_div').clone(true);
-            clone.attr('id', 'cooking_div_node');
-            last.before(clone);
-            $('#cooking_div_node #cooking_fieldset').children('#food_div').remove();
-        } else {
-            cooking_count = -1;
-            document.getElementById('cooking_div_node').remove();
-        }
+function init_alchemy_table()
+{
+    $("alchemy_add_button").on("click", function ()
+    {
+        addRow("alchemyDataTable");
     });
-    $('#cooking_add_button').on('click', function () {
-        cooking_count++;
-        var clone = $('#cooking_div #cooking_fieldset #food_div').clone(true);
-        clone.attr('id', 'food_div'.concat(cooking_count));
-        clone.insertBefore('#cooking_div_node #cooking_fieldset #cooking_button_div');
-    });
-
-    var brewery_count = -1;
-    $('#brewery_check').on('click', function () {
-        if (brewery_count == -1) {
-            brewery_count++;
-            var clone = $('#brewery_div').clone(true);
-            clone.attr('id', 'brewery_div_node');
-            last.before(clone);
-            $('#brewery_div_node #brewery_fieldset').children('#drink_div').remove();
-        } else {
-            brewery_count = -1;
-            document.getElementById('brewery_div_node').remove();
-        }
-    });
-    $('#brewery_add_button').on('click', function () {
-        brewery_count++;
-        var clone = $('#brewery_div #brewery_fieldset #drink_div').clone(true);
-        clone.attr('id', 'drink_div'.concat(brewery_count));
-        clone.insertBefore('#brewery_div_node #brewery_fieldset #brewery_button_div');
+    $("alchemy_remove_button").on("click", function ()
+    {
+        deleteRow("alchemyDataTable");
     });
 }
